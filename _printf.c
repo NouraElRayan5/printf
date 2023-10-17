4b86d417dd2cc84b6e629651bcf81f3f033cd7b1
Ahmed Eladwy <eladwy.ahmed@gmail.com>
2023-10-18 00:02:31 +0300

    update all

--- _printf.c
+++ _printf.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-
-void print_buffer(char buffer[], int *buff_ind);
-
-
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
